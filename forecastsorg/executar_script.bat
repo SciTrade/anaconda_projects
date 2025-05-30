cfc92f791fc6fca3758adeaf3a3b3beda307276e
SciTrade <scitrading@outlook.com>
2025-05-30 19:33:11 -0300

    BackUp300525

--- forecastsorg/executar_script.bat
+++ forecastsorg/executar_script.bat
@@ -5,8 +5,9 @@ cd /d C:\Users\scitr\anaconda_projects\forecastsorg
 REM Ativa o ambiente virtual
 call venv\Scripts\activate.bat
 
-REM Executa o script Python
-pythonw.exe fore_import_msj.pyw
+REM Executa o script Python sin que abra a janela cmd
+venv\Scripts\pythonw.exe fore_import_msj.pyw
+
 
 REM (Opcional) Desativa o ambiente ou aguarda o término
 REM deactivate
